da2f10272ddd935630e8979c8070d6a43501abbc
dddesire <rafael_alebb@hotmail.com>
2023-10-04 13:13:04 -0300

    adicionado cena do trovadorismo, sprites e animações do guerreiro, novas fontes para o menu e assets extras

--- game/Menu.gd
+++ game/Menu.gd
@@ -1,14 +1,12 @@
 extends Control
 
 
-
-func _on_jogar_pressed():
-	get_tree().change_scene_to_file("res://trovadorismo.tscn")
-
-
 func _on_opções_pressed():
 	get_tree().change_scene_to_file("res://opcoes.tscn")
 
 
 func _on_sair_pressed():
 	get_tree().quit()
+
+func _on_jogar_pressed():
+	get_tree().change_scene_to_file("res://trovadorismo cena/trovadorismo.tscn")
