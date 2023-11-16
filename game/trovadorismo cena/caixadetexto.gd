44baf295e464b7d0a507efdd6c853283408df2cc
Rafael <rafael_alebb@hotmail.com>
2023-11-16 18:05:43 -0300

    adicionado lapbook completo e esqueleto para proximas perguntas do trovadorismo

---
+++ game/trovadorismo cena/caixadetexto.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("caixadetexto/Moldura Texto").visible = false
+	get_node("caixadetexto/Explicacao").visible = false
+func _on_amor_pressed():
+	get_node("caixadetexto/Moldura Texto").visible = true
+	get_node("caixadetexto/Explicacao").visible = true
+	get_node("caixadetexto/Explicacao").text = "Cantiga de amor refere-se a um género
+de poesia lírica medieval galego-portuguesa
+que se centrava em temas de amor cortês. 
+Essas canções eram tipicamente escritas
+por trovadores e muitas vezes 
+expressavam as emoções e desejos do
+poeta em relação a uma pessoa amada."
+
+func _on_amigo_pressed():
+	get_node("caixadetexto/Moldura Texto").visible = true
+	get_node("caixadetexto/Explicacao").visible = true
+	get_node("caixadetexto/Explicacao").text = "Cantiga de amigo é outro género de poesia
+lírica medieval galego-portuguesa. Essas 
+canções foram escritas na perspectiva de
+uma locutora que expressa suas emoções e
+experiências, muitas vezes envolvendo um
+diálogo com sua amiga, mãe ou confidente."
+
+func _on_maldizer_pressed():
+	get_node("caixadetexto/Moldura Texto").visible = true
+	get_node("caixadetexto/Explicacao").visible = true
+	get_node("caixadetexto/Explicacao").text = "As cantigas de maldizer são canções cuja
+estrutura comporta críticas mais diretas 
+e grosseiras. Nelas, são usadas termos de
+baixo calão, como palavrões, pois o intuito
+é mesmo agredir alguém verbalmente."
+
+func _on_escarnio_pressed():
+	get_node("caixadetexto/Moldura Texto").visible = true
+	get_node("caixadetexto/Explicacao").visible = true
+	get_node("caixadetexto/Explicacao").text = "Cantigas de escárnio caracterizavam-se pela 
+sátira e pela zombaria. Esses poemas
+frequentemente empregavam humor sarcasmo
+ou até mesmo linguagem explícita para
+criticar indivíduos ou normas sociais. O tom
+de Cantigas de escárnio era mais crítico e
+satírico se comparado aos tons românticos e
+emocionais dos demais gêneros poéticos."
